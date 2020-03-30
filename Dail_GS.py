24811bbe06a3209cb479fcc08fa0dd4e22962bd6
inpgupta01 <38682010+inpgupta01@users.noreply.github.com>
2020-03-30 19:42:41 +0530

    Create Dail_GS.py
    Adding GS dialing  code

---
+++ Dail_GS.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python/
+
+import telnetlib
+import time
+
+HOST =  '10.206.100.187'
+print HOST
+cmd_1 = "dial manual 1920 7002 [sip]"
+print cmd_1
+tn = telnetlib.Telnet(HOST, 24)
+print tn
+tn.write((cmd_1+"\n").encode('ascii'))
+tn.close()
