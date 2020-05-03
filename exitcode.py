e2f52b05815d3fc878f5f54fe45c1868f179028f
inpgupta01 <38682010+inpgupta01@users.noreply.github.com>
2020-05-03 13:44:32 +0530

    Update exitcode.py

--- exitcode.py
+++ exitcode.py
@@ -13,14 +13,24 @@ def host():
         print("*****************************************************************************************")
         print("Condition 2.Checking SSH connectivity")
         print("*****************************************************************************************")
-        sys.exit(0)
+        return 0
+        # sys.exit(0)
 
     else:
         print("Validation failed...\n Please check logs for further information ")
         # cmd_8 = 'rm -rf /tmp/*'
         # stdin, stdout, stderr = ssh.exec_command(cmd_8)
         # print("\nRPMs removed successfully from /tmp/Media* directory ")
+        return 1
+        # sys.exit(1)
+
+
+def host1():
+    final_result = host()
+    if final_result == 0:
+        sys.exit(0)
+    else:
         sys.exit(1)
 
 
-host()
+host1()
