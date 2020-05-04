683cb6ebeda2486906018b349782e20812886c7d
inpgupta01 <38682010+inpgupta01@users.noreply.github.com>
2020-05-04 11:05:19 +0530

    Update 3_gs_test_lr_2048.py

--- 3_gs_test_lr_2048.py
+++ 3_gs_test_lr_2048.py
@@ -94,9 +94,9 @@ def main_function(arg):
 
     print("#####################Connecting calls on GS############## \n ")
     for ep in final_list:
-        print("Endpoint " + str(ep) + " is connecting to VMR " + str(vmr[0]) + " at line rate " + str(
-            lr[0]) + " kbps.")
-        calling(ep, vmr[0], lr[0])
+        print("Endpoint " + str(ep) + " is connecting to VMR " + str(int(vmr[0]) + " at line rate " + str(
+            int(lr[0])) + " kbps.")
+        calling(ep, int(vmr[0]), int(lr[0]))
         time.sleep(3)
 
     print("#####################Call Connection completed on GS EPs############## \n\n ")
