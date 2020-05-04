a13157d154f2240e1ab250d5f2ea74569462c8b9
inpgupta01 <38682010+inpgupta01@users.noreply.github.com>
2020-05-04 14:33:17 +0530

    Update GS_Loopback.py

--- GS_Loopback.py
+++ GS_Loopback.py
@@ -81,6 +81,8 @@ def callState(ip):
 
 def GS_call(GS_IP, VMR, LR):
     ##Dialing GS in to VMR using format  'dial auto @bandwidth @VMR'
+    print(VMR)
+    print(LR)
     cmd_1 = "dial manual " + str(int(LR)) + " " + str(int(VMR))
     print(cmd_1)
     print("\nDialing " + str(VMR) + " on GS " + GS_IP + " with LR " + str(LR) + "\n")
